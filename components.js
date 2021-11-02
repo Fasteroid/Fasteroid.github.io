3591927d00612c8ae25357ab051c57ade7c9a2e4
Fasteroid <fasteroid@hotmail.com>
2021-11-02 14:34:29 -0400

    navbar part 1

---
+++ components.js
@@ -0,0 +1,32 @@
+
+// thx stackoverflow - code below based on https://stackoverflow.com/a/31837264/15204995
+// now with less JQuery since I'm a vanilla purist!
+
+let postprocessors = {
+    /** 
+     * The postprocessor for the navbar.
+     * It finds the page we're on and perma-highlights it.
+     * @param {Element} self - imported navbar
+     */
+    navbar: function(self){
+        let pages = self.querySelector("#pages")
+        let url = window.location.href.match(/[^\/]*$/)[0]; // I'd be a fool to recompute this
+        for(let page of pages.children){
+            if( page.getAttribute("href").match(/[^\/]*$/)[0] == url ){
+                page.className = "cwd"; // "current working directory"
+            }
+        }
+    }
+}
+
+
+    let includes = document.querySelectorAll("component")
+    $.each(includes, function () {
+
+        let name = this.getAttribute("class");
+        let file = `components/${name}.html`
+        let self = this; // horrible
+        $(this).load(file, 
+            function(){ postprocessors[name](self) }    
+        )
+    })
