e15ac9c8b1dc719dc040135f3acf90e0a38aed2c
Fasteroid <fasteroid@hotmail.com>
2021-12-06 03:08:06 -0500

    tech tree v2!

--- scripts/skilltreeanimation.js
+++ scripts/skilltreeanimation.js
@@ -1,10 +1,11 @@
 const container = document.querySelector(".grid")
-container.childNodes.forEach( (child) => {
+container.querySelectorAll(".grid > div").forEach( (child,n) => {
 
     console.log(child)
 
-    if(child.firstChild.innerText != "START"){
+    if(n > 0){
         child.style.opacity = 0;
+        setTimeout(()=>{child.style.opacity = 1}, n*10)
     }
     
 } )
