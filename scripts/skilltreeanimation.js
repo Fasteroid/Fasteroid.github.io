0c594e78423e5e094130391a9e9759c81b78944d
Fasteroid <fasteroid@hotmail.com>
2021-12-09 01:24:47 -0500

    epic animations

--- scripts/skilltreeanimation.js
+++ scripts/skilltreeanimation.js
@@ -1,13 +1,25 @@
-// *aside from this really brief animation where they all appear.
 
-const container = document.querySelector(".grid")
+
+const container = document.querySelector(".grid");
 container.querySelectorAll(".grid > div").forEach( (child,n) => {
 
     console.log(child)
 
-    if(n > 0){
+    if(n > 2){
         child.style.opacity = 0;
-        setTimeout(()=>{child.style.opacity = 1}, n*10)
+        setTimeout(()=>{child.style.opacity = 1}, n*50 + 400);
+    }
+    else{
+        child.style.transform = "scale(0)";
+        child.style.webkitTransform = "scale(0)";
+        child.style.transition = "0.5s transform";
+        setTimeout(()=>{
+            child.style.webkitTransform = "scale(100%)";
+            child.style.webkitTransform  = "scale(100%)"
+        }, 200 + n*50)
+        setTimeout(()=>{
+            child.style.transition = "";
+        }, 1500)
     }
     
 } )
