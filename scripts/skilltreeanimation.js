a455db38b8ec073274c53338a453a21f115bfe09
Fasteroid <fasteroid@hotmail.com>
2021-12-09 01:29:36 -0500

    sticky navbar!

--- scripts/skilltreeanimation.js
+++ scripts/skilltreeanimation.js
@@ -2,9 +2,6 @@
 
 const container = document.querySelector(".grid");
 container.querySelectorAll(".grid > div").forEach( (child,n) => {
-
-    console.log(child)
-
     if(n > 2){
         child.style.opacity = 0;
         setTimeout(()=>{child.style.opacity = 1}, n*50 + 400);
